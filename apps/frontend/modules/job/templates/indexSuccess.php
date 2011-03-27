e8b80cb0bd3e519eb37c33a0d5bbe739cd45522a
jenkys <jan.chrastina@gmail.com>
2011-03-28 00:41:02 +0200

    Kompletni day 5.

--- apps/frontend/modules/job/templates/indexSuccess.php
+++ apps/frontend/modules/job/templates/indexSuccess.php
@@ -3,11 +3,7 @@
     <?php foreach ($jobeet_jobs as $i => $job): ?>
       <tr class="<?php echo fmod($i, 2) ? 'even' : 'odd' ?>">
         <td class="location"><?php echo $job->getLocation() ?></td>
-        <td class="position">
-          <a href="<?php echo url_for('job/show?id='.$job->getId()) ?>">
-            <?php echo $job->getPosition() ?>
-          </a>
-        </td>
+        <td class="position"><?php echo link_to($job->getPosition(), 'job_show_user', $job) ?></td>
         <td class="company"><?php echo $job->getCompany() ?></td>
       </tr>
     <?php endforeach ?>
--- lib/model/doctrine/JobeetJob.class.php
+++ lib/model/doctrine/JobeetJob.class.php
@@ -10,10 +10,27 @@
  * @author     Your name here
  * @version    SVN: $Id: Builder.php 7490 2010-03-29 19:53:27Z jwage $
  */
-class JobeetJob extends BaseJobeetJob {
+class JobeetJob extends BaseJobeetJob
+{
 
-    public function __toString() {
+    public function __toString()
+    {
         return sprintf('jenkys %s at %s (%s)', $this->getPosition(), $this->getCompany(), $this->getLocation());
+    }
+
+    public function getCompanySlug()
+    {
+        return Jobeet::slugify($this->getCompany());
+    }
+
+    public function getPositionSlug()
+    {
+        return Jobeet::slugify($this->getPosition());
+    }
+
+    public function getLocationSlug()
+    {
+        return Jobeet::slugify($this->getLocation());
     }
 
 }
